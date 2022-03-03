c6de19aac6897db12608720b159b7485042c8fd1
Davaadorj Battulga <davaadorj.battulga@u-hopper.com>
2022-03-03 10:35:10 +0100

    fix

--- g5k_conf.py
+++ g5k_conf.py
@@ -68,8 +68,8 @@ conf = (
     Configuration
         .from_settings(
         job_type="allow_classic_ssh",
-        job_name="gTestJob",
-        walltime='00:30:00'
+        job_name="iTestJob",
+        walltime='01:00:00'
         #env_name="/grid5000/images/debian9-x64-base-2020032721.tgz"
     )
         .add_network_conf(network_rennes)
