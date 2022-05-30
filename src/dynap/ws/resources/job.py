29bcbce7606178b169362acec7e9c37673254ad2
Davaadorj Battulga <davaadorj.battulga@u-hopper.com>
2022-05-30 09:41:19 +0200

    test

--- src/dynap/ws/resources/job.py
+++ src/dynap/ws/resources/job.py
@@ -77,8 +77,8 @@ class JobInterface(Resource):
                     "topic": downstream.topic,
                     "sink_address": downstream.address
                 }
-                # if downstream.address != job.agent_address:
-                requests.post(Common.HTTP + job.agent_address + Common.AGENT_PORT + "/client", json=json_data)
+                if downstream.address != job.agent_address:
+                    requests.post(Common.HTTP + job.agent_address + Common.AGENT_PORT + "/client", json=json_data)
 
             for upstream in job.upstream:
                 source_topic.append(upstream.topic)
@@ -88,8 +88,8 @@ class JobInterface(Resource):
                     "topic": upstream.topic,
                     "sink_address": job.agent_address
                 }
-                # if upstream.address != job.agent_address:
-                requests.post(Common.HTTP + upstream.address + Common.AGENT_PORT + "/client", json=json_data)
+                if upstream.address != job.agent_address:
+                    requests.post(Common.HTTP + upstream.address + Common.AGENT_PORT + "/client", json=json_data)
 
             deployed_job = DeployedJob(
                     job_name=job.job_name,
