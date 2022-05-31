9850fe56a7c59b98e570f42ce945b88214180bdb
Davaadorj Battulga <davaadorj.battulga@u-hopper.com>
2022-05-31 08:39:15 +0200

    asd

--- src/dynap/manager/client.py
+++ src/dynap/manager/client.py
@@ -15,9 +15,9 @@ class ClientManager:
 
     @staticmethod
     def on_message(client: mqtt.Client, userdata: Client, message):
-        pub_client = mqtt.Client(userdata.client_id, clean_session=False)
+        pub_client = mqtt.Client(userdata.client_id, clean_session=True)
         pub_client.connect(userdata.sink_address)
-        pub_client.publish(topic=userdata.topic, payload=str(message.payload.decode("utf-8")), qos=1, retain=False)
+        pub_client.publish(topic=userdata.topic, payload=str(message.payload.decode("utf-8")), qos=0, retain=False)
         #pub_client.disconnect()
         msg = str(message.payload.decode("utf-8"))
         logger.info(f"Client on message {msg}")
